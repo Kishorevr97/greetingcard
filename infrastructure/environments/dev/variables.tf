466b9270269e696216f059026364c6db86fd4b4d
Kishorevr97 <107535279+Kishorevr97@users.noreply.github.com>
2025-02-23 16:31:20 +0530

    Create variables.tf

---
+++ infrastructure/environments/dev/variables.tf
@@ -0,0 +1,9 @@
+variable "environment" {
+  type = string
+  description = "define environment variable"
+}
+
+variable "lambda_memory_size" {
+  description = "define lambda memory size"
+  type = number
+}
