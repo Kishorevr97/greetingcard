4f3a17790367574b6afbe8ec0c191a228fc46379
Kishorevr97 <107535279+Kishorevr97@users.noreply.github.com>
2025-02-23 12:29:54 +0530

    Create main.tf

---
+++ infrastructure/environments/dev/main.tf
@@ -0,0 +1,31 @@
+module "S3" {
+  source          = "./modules/S3"
+  src_bucket_name = "src-bucket-some-random-string"
+  dst_bucket_name = "dst-bucket-some-random-string"
+  tag_environment = var.environment
+}
+
+module "sqs" {
+  source          = "./modules/sqs"
+  tag_environment = var.environment
+}
+
+module "lambdas" { 
+    source = "./modules/lambdas"
+    depends_on = [ module.storage, module.sqs ]
+    src_bucket_arn = module.storage.src_bucket_arn
+    src_bucket_id = module.storage.src_bucket_id
+    dst_bucket_arn = module.storage.dst_bucket_arn
+    dst_bucket_id = module.storage.dst_bucket_id
+    greeting_queue_arn = module.sqs.greeting_queue_arn
+    lambda_memory_size = var.lambda_memory_size
+    tag_environment = var.environment
+}
+
+module "apigateway" {
+  source = "./modules/apigateway"
+  depends_on = [ module.sqs ]
+  greeting_queue_name = module.sqs.greeting_queue_name
+  greeting_queue_arn = module.sqs.greeting_queue_arn
+  tag_environment = var.environment
+}
